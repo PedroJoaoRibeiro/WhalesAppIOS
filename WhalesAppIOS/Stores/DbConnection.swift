0d9e87870199b47312622e1c7a94d354912b322b
PedroJoaoRibeiro <pedro_ribeiro_93@hotmail.com>
2018-05-05 02:43:03 +0100

    added isFromServer to DB

--- WhalesAppIOS/Stores/DbConnection.swift
+++ WhalesAppIOS/Stores/DbConnection.swift
@@ -19,7 +19,8 @@ class DbConnection {
     private var db: OpaquePointer?
     
     // queries statements
-    private let insertQueryString = "INSERT OR IGNORE INTO Whales (id, jsonString) VALUES (?,?)"
+    private let createtableQuerry = "CREATE TABLE IF NOT EXISTS Whales (id TEXT PRIMARY KEY, jsonString TEXT, isFromServer INTEGER)"
+    private let insertQueryString = "INSERT OR IGNORE INTO Whales (id, jsonString, isFromServer) VALUES (?,?,?)"
     private let getDataQueryString = "SELECT * FROM Whales"
     
     init() {
@@ -34,7 +35,7 @@ class DbConnection {
         }
         
         // creates db table
-        if sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS Whales (id TEXT PRIMARY KEY, jsonString TEXT)", nil, nil, nil) != SQLITE_OK {
+        if sqlite3_exec(db, createtableQuerry, nil, nil, nil) != SQLITE_OK {
             let errmsg = String(cString: sqlite3_errmsg(db)!)
             print("error creating table: \(errmsg)")
         }
@@ -43,6 +44,7 @@ class DbConnection {
     public func insertDataIntoDb(id: String, jsonString: String, isfromserver: Bool) -> Bool {
         //creating a statement
         var stmt: OpaquePointer?
+        let result = isfromserver ? 1 : 0
         
         //prepare the querry
         if sqlite3_prepare(db, insertQueryString, -1, &stmt, nil) != SQLITE_OK{
@@ -54,13 +56,19 @@ class DbConnection {
         //binding the parameters
         if sqlite3_bind_text(stmt, 1, id, -1, nil) != SQLITE_OK{
             let errmsg = String(cString: sqlite3_errmsg(db)!)
-            print("failure binding name: \(errmsg)")
+            print("failure binding id: \(errmsg)")
             return false
         }
         
         if sqlite3_bind_text(stmt, 2, jsonString, -1, nil) != SQLITE_OK{
             let errmsg = String(cString: sqlite3_errmsg(db)!)
-            print("failure binding name: \(errmsg)")
+            print("failure binding jsonString: \(errmsg)")
+            return false
+        }
+        
+        if sqlite3_bind_int(stmt, 3, Int32(result)) != SQLITE_OK{
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("failure binding isFromServer: \(errmsg)")
             return false
         }
         
