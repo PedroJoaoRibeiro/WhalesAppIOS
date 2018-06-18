366b303d3da2db1e0c1805453ba4752772cea451
PedroJoaoRibeiro <pedro_ribeiro_93@hotmail.com>
2018-06-18 15:42:44 +0100

    added selection for dates

--- WhalesAppIOS/Controllers/CubijsmViewController.swift
+++ WhalesAppIOS/Controllers/CubijsmViewController.swift
@@ -13,16 +13,139 @@ class CubijsmViewController: UIViewController {
     
     @IBOutlet weak var cubiChartView: LineChartView!
     
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var forwardButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    private var currentDate = Date()
+    
+    
     override func viewDidLoad(){
         super.viewDidLoad()
         
-        drawCubicChart()
+        dateLabel.text = "Today"
+        drawCubicChart(arrayOfData: getDateForDay())
     }
     
-    private func drawCubicChart(){
+    // Actions
+    
+    @IBAction func segmentedControllChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            break
+        case 1:
+            print("1")
+        case 2:
+            print("2")
+        case 3:
+            print("3")
+        default:
+            break
+        }
+        updateLabel()
+    }
+    
+    @IBAction func backButtonPressed(_ sender: UIButton) {
+        let calendar = Calendar.current
+        switch segmentedControl.selectedSegmentIndex {
+        case 0: //subtract a day in the currentDate
+            currentDate = calendar.date(byAdding: .day, value: -1, to: currentDate)!
+            if calendar.isDateInToday(currentDate){
+                dateLabel.text = "Today"
+            } else if calendar.isDateInYesterday(currentDate){
+                dateLabel.text = "Yesterday"
+            } else {
+                dateLabel.text = currentDate.toString(withFormat: "dd/MM/yyyy")
+            }
+            break
+        case 1: //subtract a week in the currentDate
+            currentDate = calendar.date(byAdding: .day, value: -7, to: currentDate)!
+            break
+        case 2: //subtract a month in the currentDate
+            currentDate = calendar.date(byAdding: .month, value: -1, to: currentDate)!
+            dateLabel.text = currentDate.toString(withFormat: "MM/yyyy")
+            break
+        case 3: //subtract a year in the currentDate
+            currentDate = calendar.date(byAdding: .year, value: -1, to: currentDate)!
+            dateLabel.text = currentDate.toString(withFormat: "yyyy")
+            break
+        default:
+            break
+        }
+        updateLabel()
+        print(currentDate)
+    }
+    
+    
+    @IBAction func forwardButtonPressed(_ sender: UIButton) {
+        let calendar = Calendar.current
+        switch segmentedControl.selectedSegmentIndex {
+        case 0: //add a day in the currentDate
+            currentDate = calendar.date(byAdding: .day, value: 1, to: currentDate)!
+            break
+        case 1: //add a week in the currentDate
+            currentDate = calendar.date(byAdding: .day, value: 7, to: currentDate)!
+            
+            break
+        case 2: //add a month in the currentDate
+            currentDate = calendar.date(byAdding: .month, value: 1, to: currentDate)!
+            
+            break
+        case 3: //add a year in the currentDate
+            currentDate = calendar.date(byAdding: .year, value: 1, to: currentDate)!
+            
+            break
+        default:
+            break
+        }
+        updateLabel()
+        print(currentDate)
+    }
+    
+    // Date selection Methods
+    
+    
+    private func getDateForDay() -> [DataModel] {
         //connects to the database to get the data
         let db = DbConnection()
         let arrayOfData = db.getDataFromDb()
+        
+        return arrayOfData.filter { Calendar.current.isDate($0.date, inSameDayAs: currentDate)}
+    }
+    
+    
+    
+    private func updateLabel(){
+        let calendar = Calendar.current
+        switch segmentedControl.selectedSegmentIndex {
+            case 0: // Day
+                if calendar.isDateInToday(currentDate){
+                    dateLabel.text = "Today"
+                } else if calendar.isDateInYesterday(currentDate){
+                    dateLabel.text = "Yesterday"
+                } else {
+                    dateLabel.text = currentDate.toString(withFormat: "dd/MM/yyyy")
+                }
+                break
+            case 1: // Week
+                let endOfWeek = currentDate.endOfWeek!
+                dateLabel.text = currentDate.startOfWeek!.toString(withFormat: "dd/MM/") + " - " + endOfWeek.toString(withFormat: "dd/MM/yyyy")
+                break
+            case 2: // Month
+                dateLabel.text = currentDate.toString(withFormat: "MM/yyyy")
+            case 3: // Year
+                dateLabel.text = currentDate.toString(withFormat: "yyyy")
+                break
+            default:
+                break
+            }
+    }
+    
+    
+    // Chart Methods
+    
+    private func drawCubicChart(arrayOfData: [DataModel]){
         
         setChartOptions();
         
@@ -97,6 +220,12 @@ class CubijsmViewController: UIViewController {
         
         cubiChartView.animate(xAxisDuration: 1)
     }
+    
+    
+    
+    
+    
+    
 }
 
 private class CubicLineSampleFillFormatter: IFillFormatter {
--- WhalesAppIOS/Ressources/Extensions.swift
+++ WhalesAppIOS/Ressources/Extensions.swift
@@ -29,6 +29,18 @@ extension DateFormatter {
 
 extension Date {
     
+    var startOfWeek: Date? {
+        let gregorian = Calendar(identifier: .gregorian)
+        guard let sunday = gregorian.date(from: gregorian.dateComponents([.yearForWeekOfYear, .weekOfYear], from: self)) else { return nil }
+        return sunday
+    }
+    
+    var endOfWeek: Date? {
+        let gregorian = Calendar(identifier: .gregorian)
+        guard let sunday = gregorian.date(from: gregorian.dateComponents([.yearForWeekOfYear, .weekOfYear], from: self)) else { return nil }
+        return gregorian.date(byAdding: .day, value: 7, to: sunday)
+    }
+    
     func toString(withFormat format: String) -> String {
         let formatter = DateFormatter()
         formatter.dateFormat = format
@@ -38,6 +50,8 @@ extension Date {
         
         return formatter.string(from: yourDate!)
     }
+    
+    
 }
 
 extension UIColor {
